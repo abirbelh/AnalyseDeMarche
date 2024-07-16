25a8fef33224f4d73b1e7a1334e18f4303492c15
abirbelh <abirbelhedi8@gmail.com>
2024-07-16 11:28:18 +0100

    authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,13 +1,18 @@
-// middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication failed!' });
+  }
   try {
-    const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'your_jwt_secret');
-    req.utilisateurData = { utilisateurId: decodedToken.utilisateurId, role: decodedToken.role };
+    req.utilisateurId = decodedToken.utilisateurId;
+    req.role = decodedToken.role;
     next();
   } catch (error) {
-    res.status(401).json({ message: 'Authentication failed!' });
+    res.status(401).json({ message: 'Invalid token!' });
   }
 };
+
+module.exports = authMiddleware;
